835f2f10670a5337dd8ade16d5bce09bcb310fdf
Adriana <adriana.calvomatos@gmail.com>
2021-09-18 23:50:41 +0000

    completed sum a to b

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  // if start and end values are equal, return
+  if (fromN === toN) {
+    return fromN;
+  } else {
+    return fromN + sum(fromN + 1, toN);
+  }
 }
 
 module.exports = sum;
+
+// sum(3 ,5)
+//   3 + sum(4, 5)
+//     4 + sum(5,5)
+//     return 4 + 5
+//   return 3 + 4 + 5
+// return
